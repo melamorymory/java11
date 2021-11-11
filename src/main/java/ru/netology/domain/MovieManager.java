1955697558327dfc79c2792e5e14248fbdce3b79
Olga Lebedeva <birdswann@gmail.com>
2021-11-11 21:45:57 +0300

    Task 2 done

--- src/main/java/ru/netology/domain/MovieManager.java
+++ src/main/java/ru/netology/domain/MovieManager.java
@@ -1,40 +1,28 @@
 package ru.netology.domain;
 
 public class MovieManager {
-    private int length;
+    private MovieRepo repository;
 
-    public MovieManager(int length) {
-        this.length = length;
+    public MovieManager(MovieRepo repository) {
+        this.repository = repository;
     }
-
-    public MovieManager() {
-    }
-
-    private MovieItem[] items = new MovieItem[0];
 
     public void add(MovieItem item) {
-        MovieItem[] tmp = new MovieItem[items.length + 1];
-        for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[i];
-        }
-        tmp[tmp.length - 1] = item;
-        items = tmp;
+        repository.save(item);
     }
 
     public MovieItem[] getAll() {
-        int resultLength;
-        if (length == 0){
-            resultLength = 10;
-        }
-        else {
-            resultLength = length;
-        }
-        MovieItem[] result = new MovieItem[resultLength];
-        for (int i = 0; i < resultLength; i++) {
-            int index = resultLength - i - 1;
+        MovieItem[] items = repository.findAll();
+        MovieItem[] result = new MovieItem[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
     }
 
 
--- src/test/java/ru/netology/domain/MovieManagerTest.java
+++ src/test/java/ru/netology/domain/MovieManagerTest.java
@@ -1,66 +1,56 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 class MovieManagerTest {
+    @Mock
+    private MovieRepo repository = Mockito.mock(MovieRepo.class);
+    @InjectMocks
+    private MovieManager manager;
+    private MovieItem first = new MovieItem(1,"Дюна","фантастика",10);
+    private MovieItem second = new MovieItem(2,"Вечные","фантастика",20);
+    private MovieItem third = new MovieItem(3,"Не время умирать","боевик",30);
+
+//    @Test
+//    public void shouldAdd() {
+//        MovieItem[] returned = {first};
+//        doReturn(returned).when(repository).save(first);
+//
+//        manager.add(first);
+//        MovieItem[] actual = manager.getAll();
+//        MovieItem[] expected = new MovieItem[]{first};
+//        assertArrayEquals(actual, expected);
+//    }
 
     @Test
-    void shouldReturn10Movies() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem(1,"Дюна","фантастика",10);
-        MovieItem second = new MovieItem(2,"Вечные","фантастика",20);
-        MovieItem third = new MovieItem(3,"Не время умирать","боевик",30);
-        MovieItem fourth = new MovieItem(4,"Веном 2","фантастика",40);
-        MovieItem fifth = new MovieItem(5,"Неисправимый Рон","Мультфильм",50);
-        MovieItem sixth = new MovieItem(6,"Ряд 19","триллер",60);
-        MovieItem seventh = new MovieItem(7,"Друг на продажу","комедия",70);
-        MovieItem eighth = new MovieItem(8,"Семейка Аддамс","мультфильм",80);
-        MovieItem ninth = new MovieItem(9,"Ледяной демон","ужасы",90);
-        MovieItem tenth = new MovieItem(10,"Вкус жизни","драма",100);
-        manager.add(first);
-        manager.add(second);
-        manager.add(third);
-        manager.add(fourth);
-        manager.add(fifth);
-        manager.add(sixth);
-        manager.add(seventh);
-        manager.add(eighth);
-        manager.add(ninth);
-        manager.add(tenth);
-        MovieItem[] actual = manager.getAll();
-        MovieItem[] expected = new MovieItem[]{tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+    public void shouldGetAll() {
+        MovieItem[] returned = {first, second, third};
+        doReturn(returned).when(repository).findAll();
 
-        assertArrayEquals(actual,expected);
+        MovieItem[] actual = manager.getAll();
+        MovieItem[] expected = new MovieItem[]{third, second, first};
+        assertArrayEquals(actual, expected);
     }
 
-    @Test
-    void shouldReturn5Movies() {
-        MovieManager manager = new MovieManager(5);
-        MovieItem first = new MovieItem(1,"Дюна","фантастика",10);
-        MovieItem second = new MovieItem(2,"Вечные","фантастика",20);
-        MovieItem third = new MovieItem(3,"Не время умирать","боевик",30);
-        MovieItem fourth = new MovieItem(4,"Веном 2","фантастика",40);
-        MovieItem fifth = new MovieItem(5,"Неисправимый Рон","Мультфильм",50);
-        MovieItem sixth = new MovieItem(6,"Ряд 19","триллер",60);
-        MovieItem seventh = new MovieItem(7,"Друг на продажу","комедия",70);
-        MovieItem eighth = new MovieItem(8,"Семейка Аддамс","мультфильм",80);
-        MovieItem ninth = new MovieItem(9,"Ледяной демон","ужасы",90);
-        MovieItem tenth = new MovieItem(10,"Вкус жизни","драма",100);
-        manager.add(first);
-        manager.add(second);
-        manager.add(third);
-        manager.add(fourth);
-        manager.add(fifth);
-        manager.add(sixth);
-        manager.add(seventh);
-        manager.add(eighth);
-        manager.add(ninth);
-        manager.add(tenth);
-        MovieItem[] actual = manager.getAll();
-        MovieItem[] expected = new MovieItem[]{fifth, fourth, third, second, first};
-
-        assertArrayEquals(actual,expected);
-    }
+//    @Test
+//    public void shouldRemoveById() {
+//        MovieItem[] returned = {first, third};
+//        doReturn(returned).when(repository).removeById(2);
+//
+//        manager.add(first);
+//        manager.add(second);
+//        manager.add(third);
+//        manager.removeById(2);
+//        MovieItem[] actual = manager.getAll();
+//        MovieItem[] expected = new MovieItem[]{third, first};
+//    }
 }
